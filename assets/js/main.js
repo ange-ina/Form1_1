92b7fa0907e38881e5c51eded9063fcd8f48da1b
ange-ina <heinrich.inna@gmail.com>
2020-06-17 14:40:02 +0200

    Form1_1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let firstName = document.getElementById('first-name')
+let lastName = document.getElementsById('last-name')
+let handy = document.getElementsById('handy')
+let email = document.getElementsById('email')
+let password = document.getElementsById('password')
+
+function showInput() {
+    console.log("it works")
+    console.log(firstName.value)
+    console.log(lastName.value)
+    console.log(handy.value)
+    console.log(email.value)
+    console.log(password.value)
+}
+console.log(document.getElementsById("headline").innerHTML)
